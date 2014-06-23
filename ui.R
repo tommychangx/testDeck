43bd9f7ee04b7868901f05be558f32c1ac744225
tommychangx <tommychangx@users.noreply.github.com>
2014-06-24 05:46:48 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,26 +8,31 @@ shinyUI(pageWithSidebar(
 
   # Sidebar 
   sidebarPanel(
+	numericInput("inputvalue1", "result = sample * 3", 10,
+			 min = 1, max = 100),
+  
     selectInput("variable", "Car Variable:",
                 list("Number of cylinders" = "cyl", 
                      "Transmission (0 = automatic, 1 = manual)" = "am", 
                      "Number of forward gears" = "gear")),
 
     checkboxInput("outliers", "Show Regression Line", FALSE),
-	helpText("Online Doc : Select Number of cylinders or Transmission or Number of forward gears to show on the screen, click or unclick to show regression line or not")
+	helpText("Online Doc : \n
+    Select Numeric Value, it will be multipled by 3 times \n	
+	Select 'Number of cylinders' or 'Transmission' or 'Number of forward gears' to show on the screen, \n
+	click or unclick to show regression line or not \n
+	")
 	
   ),
 
   # Show the caption and plot
   mainPanel(
-   
-    h2("Motor Trend Car Road Tests"),
+
+    h1("Result : "),
+    h1(textOutput("calvalue1")),
   
-    h4("mpg - Miles/(US) gallon"),
-    h4("cyl - Number of cylinders"),
-    h4("am - Transmission (0 = automatic, 1 = manual)"),
-    h4("gear - Number of forward gears"),   
-   
+    h4("Motor Trend Car Road Tests"),
+
     h3(textOutput("caption")),
     plotOutput("mpgPlot")
   )
